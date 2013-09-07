0bf8c62a16831b50d558def9fb49c459c492d128
Kai Lieth <git@radiant-cove-7007>
2013-09-07 02:26:33 -0400

    Shell stays synched with textarea

--- libs/shell.js
+++ libs/shell.js
@@ -17,18 +17,42 @@ var Shell = (function() {
 	var original;
 
 	// Stores the canvas (visible) and the text area (hidden)
-	var canvas;
+	var canvas, ctx, width = 800, height = 400;
 	var text;
+
+	var V_PADDING = 5;
+	var LINE_HEIGHT = 12;
+
+	var redrawCanvas = function() {
+		ctx.fillStyle = "rgb(0,0,0)";
+		ctx.fillRect(0,0,width,height);
+		ctx.fillStyle = "rgb(255,255,255)";
+		var str = text.val().split("\n");
+		var y = LINE_HEIGHT + V_PADDING;
+		$.each(str,function(i, item) {
+			// TODO - if item length is longer than width, split it into multiple lines.
+			ctx.fillText(item, 2, y);
+			y += LINE_HEIGHT + V_PADDING;
+		});
+	};
+
 	return function(id) {
 		// Remove the specified id, replace with canvas
 		original = $('#' + id);
-		canvas = $("<canvas></canvas>");
-		text = $("<textarea></textarea>");
+		canvas = $("<canvas></canvas>").prop({width:width,height:height});
 		canvas.attr('id',ShellUtils.getNewShellID());
+		ctx = canvas[0].getContext('2d');
+		text = $("<textarea></textarea>");
 		original.parent().append(canvas);
 		original.parent().append(text);
 		original.remove();
 		delete original;
+
+		// set up canvas
+		ctx.font = "20px Courier";
+		ctx.fillStyle = "rgb(0,0,0)";
+		ctx.fillRect(0,0,width,height);
+		ctx.fillStyle = "rgb(255,255,255)";
 
 		// callbacks for canvas
 		canvas.click(function() {
@@ -36,5 +60,9 @@ var Shell = (function() {
 		});
 
 		// callbacks for text
+		text.keyup(function(e) {
+			// TODO - if the value is more lines than I can handle, shift it and save.
+			redrawCanvas();
+		});
 	};
 })();
